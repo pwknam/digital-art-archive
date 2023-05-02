de42c2ceba8aa1ff2147303912ff826f080b86eb
Kyushik <kyushik95@gmail.com>
2023-05-02 12:23:16 -0400

    before I begin playing around with a different way to add artworks

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { useRouter } from "next/navigation";
 
 export default function DeleteButton({ artwork }) {
-  const router = useRouter();
   console.log(artwork.collection.id);
   function handleDelete() {
     console.log(artwork.id);
     fetch(`/api/artwork/${artwork.id}?method=DELETE`, {
       method: "DELETE",
     });
-    router.push(`/collection/${artwork.collection.id}`);
+    window.location.href = `/collection/${artwork.collection.id}`;
   }
   return <button onClick={handleDelete}>Delete Artwork</button>;
 }
